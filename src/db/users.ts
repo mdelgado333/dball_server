05699dcbecf54f9b2e54b1a909b94b108614a76d
Mike Delgado <mikedelgado@macbook-pro-de-mike-1.home>
2025-01-15 10:38:40 +0100

    login route + adding sessionToken to the UserSchema

--- src/db/users.ts
+++ src/db/users.ts
@@ -5,7 +5,8 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true },
     authentication: {
         password: { type: String, required: true, select: false },
-        salt: { type: String, select: false }
+        salt: { type: String, select: false },
+        sessionToken: { type: String, select: false }
     }
 })
 
