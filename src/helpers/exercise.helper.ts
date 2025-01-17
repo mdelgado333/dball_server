a3c69b42ccbc8bb61491b41080e90ce4c25765bb
Mike Delgado <mikedelgado@macbook-pro-de-mike-1.home>
2025-01-17 17:22:33 +0100

    exercise helpers for createExercise, updateExerciseById and deleteExerciseById

--- src/helpers/exercise.helper.ts
+++ src/helpers/exercise.helper.ts
@@ -2,3 +2,6 @@ import { ExerciseModel } from "../models/Exercise.model";
 
 export const getExercises = () => ExerciseModel.find()
 export const getExerciseById = (id: string) => ExerciseModel.findById(id)
+export const createExercise = (values: Record<string, any>) => new ExerciseModel(values)
+export const updateExerciseById = (id: string, values: Record<string, any>) => ExerciseModel.findByIdAndUpdate(id, values)
+export const deleteExerciseById = (id: string) => ExerciseModel.findByIdAndDelete({_id: id})
