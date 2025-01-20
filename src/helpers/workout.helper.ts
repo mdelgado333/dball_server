77f9ee4f7b95f8a6d0d1b93bb030432fe407c6d5
Mike Delgado <mikedelgado@macbook-pro-de-mike-1.home>
2025-01-20 09:18:15 +0100

    workout helpers

--- src/helpers/workout.helper.ts
+++ src/helpers/workout.helper.ts
@@ -1,8 +1,8 @@
 import { WorkoutModel } from "models/Workout.model";
 
 export const getWorkouts = () => WorkoutModel.find()
-// export const getUserById = (id: string) => UserModel.findById(id)
-// export const createUser = (values: Record<string, any>) => new UserModel(values)
-//     .save().then((user) => user.toObject())
+export const getWorkoutById = (id: string) => WorkoutModel.findById(id)
+export const createWorkout = (values: Record<string, any>) => new WorkoutModel(values)
+     .save().then((workout) => workout.toObject())
+export const updateWorkoutById = (id: string, values: Record<string, any>) => WorkoutModel.findByIdAndUpdate(id, values)
 export const deleteWorkoutById = (id: string) => WorkoutModel.findOneAndDelete({ _id: id })
-// export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values)
