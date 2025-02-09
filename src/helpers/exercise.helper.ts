045703569421e10ede2fa79342f4075dc4b2a9ee
Mike Delgado <mikedelgado@macbook-pro-de-mike-1.home>
2025-02-09 14:03:30 +0100

    deleted exercise helpers and controllers to centralize everything on exercise routes

--- src/helpers/exercise.helper.ts
+++
@@ -1,8 +0,0 @@
-import { Exercise } from "../models/Exercise.model";
-
-export const getExercises = () => Exercise.find()
-export const getExerciseById = (id: string) => Exercise.findById(id)
-export const createExercise = (values: Record<string, any>) => new Exercise(values)
-    .save().then((exercise) => exercise.toObject())
-export const updateExerciseById = (id: string, values: Record<string, any>) => Exercise.findByIdAndUpdate(id, values)
-export const deleteExerciseById = (id: string) => Exercise.findOneAndDelete({_id: id})
--- src/routes/exercise.routes.ts
+++ src/routes/exercise.routes.ts
@@ -1,10 +1,90 @@
 import express from "express"
-import { deleteExercise, getAllExercises, getExercise, newExercise, updateExercise } from "../controllers/exercise.controller"
+import { Exercise } from '../models/Exercise.model'
 
 export default (router: express.Router) => {
-    router.get('/exercises', getAllExercises)
-    router.get('/exercises/:id', getExercise)
-    router.post('/exercises/newExercise', newExercise)
-    router.patch('/exercises/:id', updateExercise) 
-    router.delete('/exercises/:id', deleteExercise) 
+
+    router.get('/exercises', async (req: express.Request, res: express.Response) => {
+
+        try {
+            const exercises = await Exercise.find()
+            return res.status(200).json(exercises)
+        } catch (error) {
+            console.log(error)
+            res.sendStatus(400)
+        }
+    })
+
+    router.get('/exercises/:id', async (req: express.Request, res: express.Response) => {
+
+        const { id } = req.params
+
+        try {
+            const exercise = await Exercise.findById(id)
+            return res.status(200).json(exercise)
+        } catch (error) {
+            console.log(error)
+            res.sendStatus(400)
+        }
+
+    })
+
+    router.post('/exercises/newExercise', async (req: express.Request, res: express.Response) => {
+
+        const {title, subtitle, description, sets, reps, minutes, seconds, vertical, typeOfContent } = req.body
+
+        try {
+
+            const newExercise = new Exercise({
+                info:{ title, subtitle, description },
+                details:{ sets, reps, rest: { minutes, seconds } },
+                dballInfo: { vertical, typeOfContent }
+            })
+
+            await newExercise.save()
+            return res.status(200).json(newExercise)
+            
+        } catch (error) {
+            console.log(error)
+            res.sendStatus(400)
+        }
+
+    })
+
+    router.patch('/exercises/:id', async (req: express.Request, res: express.Response) => {
+
+        const { id } = req.params
+        const { title, subtitle, description, sets, reps, minutes, seconds, vertical, typeOfContent } = req.body
+    
+        try {
+
+            const updatedExercise = await Exercise.findByIdAndUpdate(id, {
+                info:{ title, subtitle, description },
+                details:{ sets, reps, rest: { minutes, seconds } },
+                dballInfo: { vertical, typeOfContent }
+            }, {new: true})
+
+            await updatedExercise.save()
+
+            return res.status(200).json(updatedExercise)
+        
+        } catch (error) {
+            console.log(error)
+            res.sendStatus(400)
+        }
+    }) 
+
+    router.delete('/exercises/:id', async (req: express.Request, res: express.Response) => {
+        
+        const { id } = req.params
+        
+            try {
+
+                const deletedExercise = await Exercise.findOneAndDelete({_id: id})
+                return res.status(200).json(deletedExercise)
+
+            } catch (error) {
+                console.log(error)
+                res.sendStatus(400)
+            }
+    }) 
 }
