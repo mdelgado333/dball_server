168c30e26dbc951832619d01f6e6b31acb89f4f5
Mike Delgado <mikedelgado@macbook-pro-de-mike-1.home>
2025-02-09 13:26:07 +0100

    fixed /api

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-import express from 'express'
 import http from 'http'
-import bodyParser from 'body-parser'
-import cookieParser from 'cookie-parser'
-import compression from 'compression'
-import cors from 'cors'
-import mongoose from 'mongoose'
-
 import router from './routes/index.routes'
 
 import { app } from './config/index'
@@ -15,4 +8,4 @@ export const server = http.createServer(app)
 import './server'
 import './db'
 
-app.use('/', router())
+app.use('/api', router())
