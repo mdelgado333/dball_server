dd74ec487f4398bdbc60e029f930c3292f05e7c6
Mike Delgado <mikedelgado@macbook-pro-de-mike-1.home>
2025-02-09 14:50:44 +0100

    deleted learning helpers and controllers to centralize everything on learning routes

--- src/helpers/learning.helper.ts
+++
@@ -1,8 +0,0 @@
-import { Learning } from "../models/Learning.model";
-
-export const createMicroLearning = (values: Record<string,any>) => new Learning(values)
-    .save().then((microlearning)=> microlearning.toObject())
-export const getMicrolearningById = (id: string) => Learning.findById(id)
-export const getMicroLearnings = () => Learning.find()
-export const updateMicroLearningById = (id: string, values: Record<string,any>) => Learning.findByIdAndUpdate(id, values)
-export const deleteMicroLearningById = (id: string) => Learning.findOneAndDelete({ _id: id })
--- src/routes/learning.routes.ts
+++ src/routes/learning.routes.ts
@@ -1,10 +1,113 @@
 import express from 'express'
-import { deleteMicroLearning, getAllMicrolearnings, getMicrolearning, newMicroLearning, updateMicroLearning } from '../controllers/learning.controller'
+import { Learning } from '../models/Learning.model'
 
 export default (router: express.Router) => {
-    router.post('/microlearnings/newMicroLearning', newMicroLearning)
-    router.get('/microlearnings/:id', getMicrolearning)
-    router.get('/microlearnings', getAllMicrolearnings)
-    router.patch('/microlearnings/:id', updateMicroLearning) // funciona pero me pasa el json antes de editar porque he usado findByIdAndUpdate en vez de findById y después lo updateo yo, ya lo arreglaré
-    router.delete('/microlearnings/:id', deleteMicroLearning)
+
+    router.post('/learnings/newLearning', async (req: express.Request, res: express.Response) => {
+
+        const { title, subtitle, description, vertical, typeOfContent } = req.body
+
+        try {
+            
+            const newLearning = new Learning({
+                info: { title,
+                        subtitle,
+                        description
+                        },
+                dballInfo: {
+                        vertical,
+                        typeOfContent
+                        }
+            })
+        
+            await newLearning.save()
+            return res.status(200).json(newLearning)
+
+        } catch (error) {
+            console.log(error)
+            res.sendStatus(400)
+        }
+
+    })
+
+    router.get('/learnings/:id', async (req: express.Request, res: express.Response) => {
+
+        const { id } = req.params
+        
+            try {
+        
+                const learning = await Learning.findById(id)
+                return res.status(200).json(learning)
+        
+            } catch (error) {
+        
+                console.log(error)
+                res.sendStatus(400)
+        
+            }
+    })
+
+    router.get('/learnings', async (req: express.Request, res: express.Response) => {
+
+        try {
+        
+                const learnings = await Learning.find()
+                return res.status(200).json(learnings)
+        
+            } catch (error) {
+        
+                console.log(error)
+                res.sendStatus(400)
+        
+            }
+
+    })
+
+    router.patch('/learnings/:id', async (req: express.Request, res: express.Response) => {
+
+        const { id } = req.params
+        const { title, subtitle, description, vertical, typeOfContent } = req.body
+
+        try {
+
+            const updatedLearning = await Learning.findByIdAndUpdate( id, {
+                info: { title,
+                    subtitle,
+                    description
+                },
+                dballInfo: {
+                    vertical,
+                    typeOfContent
+                }
+            }, {new: true})
+
+            updatedLearning.save()
+
+            return res.status(200).json(updatedLearning)
+
+        } catch (error) {
+
+            console.log(error)
+            res.sendStatus(400)
+
+        }
+
+    })
+
+
+
+    router.delete('/learnings/:id', async (req: express.Request, res: express.Response) => {
+        
+        const { id } = req.params
+    
+        try {
+    
+            const deletedLearning = await Learning.findOneAndDelete({ _id: id })
+            return res.status(200).json(deletedLearning)
+            
+        } catch (error) {
+            console.log(error)
+            res.sendStatus(400)
+        }
+    })
 }
