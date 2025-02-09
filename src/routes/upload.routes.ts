8b863ea9fefd7b06e74ce7b38e3e99ea8c84e4a4
Mike Delgado <mikedelgado@MacBook-Pro-de-Mike.local>
2025-02-09 21:11:47 +0100

    changes in Cloudinary upload routes but still not finished

--- src/routes/upload.routes.ts
+++ src/routes/upload.routes.ts
@@ -1,7 +1,24 @@
 import express from 'express'
-import { upload, uploadToCloudinary } from '../config/cloudinary.config'
-import { imageUpload } from '../controllers/upload.controller'
+import {upload, uploadToCloudinary } from '../config/cloudinary.config'; //the file path where you had written this functions in earlier
 
 export default (router: express.Router) => {
-    router.post('/upload', upload.array('images', 5), uploadToCloudinary, imageUpload)
+
+    router.post("/upload", upload.array('images', 5),  uploadToCloudinary, async (req: express.Request, res: express.Response) => {
+        
+        const { cloudinaryUrls } = req.body
+
+        try {
+            if (cloudinaryUrls.length === 0) {
+                console.error('No Cloudinary URLs found.');
+                return res.status(500).send('Internal Server Error');
+            }
+           const images = cloudinaryUrls;
+           return res.send(images)
+    
+        } catch (error) {
+            console.log(error)
+            res.sendStatus(500).json(error)
+        }
+    });
+
 }
